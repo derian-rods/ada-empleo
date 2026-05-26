f2193ad1e0f540de0a554048b8d4681ad39fbbde
CCV Dashboard <dev@ccv-dashboard.local>
2026-05-26 22:12:06 +0200

    Remove debug code from App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,8 @@ import AppLayout from './components/AppLayout.vue'
 import CsvUploadPanel from './components/CsvUploadPanel.vue'
 import TabsView from './components/TabsView.vue'
 import { useDashboardStore } from './stores/dashboard'
-import { watch } from 'vue'
 
 const store = useDashboardStore()
-
-// Debug
-watch(
-  () => store.hasData,
-  (newVal) => {
-    console.log('hasData changed to:', newVal)
-    console.log('parentsLoaded:', store.parentsLoaded)
-    console.log('timeEntriesLoaded:', store.timeEntriesLoaded)
-    console.log('parents count:', store.parents.length)
-    console.log('timeEntries count:', store.timeEntries.length)
-    console.log('calculatedRequests count:', store.calculatedRequests.length)
-  }
-)
 </script>
 
 <template>
@@ -26,15 +12,10 @@ watch(
     <div class="app-content">
       <CsvUploadPanel />
 
-      <div v-if="store.hasData">
-        <p style="color: green; font-weight: bold;">DEBUG: hasData es TRUE</p>
-        <p>Parents: {{ store.parents.length }}, Calculados: {{ store.calculatedRequests.length }}</p>
-        <TabsView />
-      </div>
+      <TabsView v-if="store.hasData" />
 
-      <div v-else class="empty-state">
+      <div v-if="!store.hasData" class="empty-state">
         <p>Carga los tres CSV para empezar el análisis</p>
-        <p style="color: gray; font-size: 0.9rem;">DEBUG: hasData={{ store.hasData }}, parentsLoaded={{ store.parentsLoaded }}, timeEntriesLoaded={{ store.timeEntriesLoaded }}</p>
       </div>
     </div>
   </AppLayout>
